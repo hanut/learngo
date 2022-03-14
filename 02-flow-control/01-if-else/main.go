8818b779adbba56e40382bc785cdfaa66dbc0e35
Hanut Singh Gusain <hanutsingh@gmail.com>
2022-03-14 17:11:49 +0530

    updated loops & control flow examples

--- 02-flow-control/01-if-else/main.go
+++ 02-flow-control/01-if-else/main.go
@@ -2,20 +2,40 @@ package main
 
 import "fmt"
 
+type Person struct {
+	Name   string
+	Age    uint8
+	Salary float32
+}
+
 func main() {
 	rating := 7
 
 	if rating < 5 {
-		fmt.Println("Not that great of a rating:", rating)
+		fmt.Printf("A rating of %d is not that great:\n", rating)
 	} else {
-		fmt.Println("Superb rating :", rating)
+		fmt.Printf("%d is a superb rating !\n", rating)
 	}
 
 	// Simple if on a boolean value
-	hasBeans := true
+	isReady := true
 
-	if hasBeans {
-		fmt.Println("My man has beans !!!")
+	if isReady {
+		fmt.Println("Something is ready !!!")
+	} else {
+		fmt.Println("Something is not ready !!!")
 	}
 
+	// If with multiple conditions
+	p1 := Person{
+		Name:   "Alice",
+		Salary: 90000,
+		Age:    32,
+	}
+
+	if p1.Age > 30 && p1.Salary < 100000 {
+		fmt.Println(p1.Name + " can do better")
+	} else {
+		fmt.Println(p1.Name + " is doing amazing")
+	}
 }
--- 02-flow-control/02-switch/main.go
+++ 02-flow-control/02-switch/main.go
@@ -5,6 +5,7 @@ import "fmt"
 func main() {
 	y := "necklace"
 
+	// Simple switch statement
 	switch y {
 	case "necklace":
 		fmt.Println("Its a necklace")
@@ -18,4 +19,28 @@ func main() {
 		fmt.Println("I dont know what that is")
 	}
 
+	// Switch with conditions
+	sunIsShining := true
+	weatherIsSweet := true
+
+	switch {
+	case sunIsShining:
+		fmt.Println("sunIsShining:", sunIsShining) // Only this one is executed because its the first match
+	case weatherIsSweet:
+		fmt.Println("Wont reach here") // Never reach here because switch exits after first match
+	case sunIsShining && weatherIsSweet:
+		fmt.Println("Wont reach here either") // Never going to come here either
+	}
+
+	// Same conditional switch with fallthru
+	switch {
+	case sunIsShining:
+		fmt.Println("The sun is shining.") // Only this one is executed because its the first match
+		fallthrough
+	case weatherIsSweet:
+		fmt.Println("The weather is sweet, yeah.")
+		fallthrough
+	case sunIsShining && weatherIsSweet:
+		fmt.Println("Make you wanna move your dancing feet now.")
+	}
 }
